741b093a119a6146b0f432aba4cbeaf0ba4c35b1
Thomas <thomasmbise@gmail.com>
2017-09-25 17:10:56 -0400

    Add user routes [only users#create complete]

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,42 @@
 class UsersController < ApplicationController
+  def index
+    users = User.all
+
+    render json: users
+  end
+
+  def show
+    user = User.find_by(id: params[:id])
+
+    unless user == nil
+      submitted_recipes = user.submitted_recipes
+      rated_recipes = user.rated_recipes
+
+      user_data = {
+        user_info: user,
+        submitted_recipes: submitted_recipes,
+        rated_recipes: rated_recipes
+      }
+      render json: user_data
+    else
+      #if user does NOT exist, nill is returned
+      render json: user
+    end
+  end
+
+
+  def create
+    binding.pry
+    user = User.new(user_params)
+
+    user.save
+
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :username, :email, :password)
+  end
+
+
 end
