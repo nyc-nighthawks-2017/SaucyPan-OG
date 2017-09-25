488f2eb08171856200cc856e58b2c4fcd9c0d699
Caitlin Prentke <caitlin.prentke@gmail.com>
2017-09-25 15:28:38 -0400

    Correct associations to join recipes and instructions

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -4,6 +4,8 @@ class Recipe < ActiveRecord::Base
   has_many :ingredients, through: :ingredients_recipes
   has_many :ratings
   has_many :raters, through: :ratings, source: :rater
-
+  has_many :instructions_recipes
+  has_many :instructions, through: :instructions_recipes
+  
   validates :name, :category, :difficulty_level, :prep_time, :creator, presence: true
 end
