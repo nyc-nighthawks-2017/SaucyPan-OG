a0675fb36a7725d14477f077d655f904262908ca
Adam Weil <adamjweil@gmail.com>
2017-09-25 11:00:48 -0400

    updating repo

--- README.md
+++ README.md
@@ -1,11 +1,12 @@
 #Contract:
-  Core Hours—>
+Core Hours—>
+
 - When working independently, have stand ups every two hours
 - Rebecca teaches Tuesday AM
 - Adam has to leave at 530 on Tuesday
 - Thomas works best taking breaks every 25 minutes
 
-  Conflict Resolution —>
+Conflict Resolution —>
 - Spend no more than 20 minutes making a single decision.
 - Time box when appropriate.
 - If conflicts arise take vote.
--- config/application.rb
+++ config/application.rb
@@ -9,14 +9,14 @@ require "action_controller/railtie"
 require "action_mailer/railtie"
 require "action_view/railtie"
 require "action_cable/engine"
-require "sprockets/railtie"
-# require "rails/test_unit/railtie"
+# require "sprockets/railtie"
+require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module SaucyPanPg
+module SaucyPan
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
@@ -25,7 +25,9 @@ module SaucyPanPg
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
-    # Don't generate system test files.
-    config.generators.system_tests = nil
+    # Only loads a smaller set of middleware suitable for API only apps.
+    # Middleware like session, flash, cookies can be added back manually.
+    # Skip views, helpers and assets when generating a new resource.
+    config.api_only = true
   end
 end
