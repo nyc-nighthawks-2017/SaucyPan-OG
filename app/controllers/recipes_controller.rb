7976ededdc39913e3d7ef615953a1b0a0d3fd039
Caitlin Prentke <caitlin.prentke@gmail.com>
2017-09-26 20:12:49 -0400

    Sending body as json and receiving cors response

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -15,16 +15,16 @@ class RecipesController < ApplicationController
 		puts params
 		# Parameters: {"recipeName"=>"blah", "category"=>"Appetizer", "difficultyLevel"=>"5", "ingredient"=>"nada", "prepTime"=>"2", "directions"=>"nada", "creator"=>"nada"}
 		@recipe = Recipe.new
-    	@recipe.name = params[:recipeName]
-   	 	@recipe.category = params[:category]
-	    @recipe.difficulty_level = params[:difficultyLevel]
-	    @recipe.instructions.push(Instruction.create(step: params[:directions], position: 1))
-			# @recipe.ingredients.push(Ingredient.create(name: params[:ingredient]))
-	    @recipe.prep_time = params[:prepTime]
-	    @recipe.creator = params[:creator]
+    	@recipe.name = params[:recipe][:recipeName]
+   	 	@recipe.category = params[:recipe][:category]
+	    @recipe.difficulty_level = params[:recipe][:difficultyLevel]
+	    @recipe.instructions.push(Instruction.create(step: params[:recipe][:directions], position: 1))
+			# @recipe.ingredients.push(Ingredient.create(name: params[:recipe][:ingredient]))
+	    @recipe.prep_time = params[:recipe][:prepTime]
+	    @recipe.creator = params[:recipe][:creator]
 	    @recipe.submitter = User.first
 	    if @recipe.save
-				render json: @recipe.instructions
+				render json: @recipe
 			else
 				puts @recipe.errors.full_messages
 			end
